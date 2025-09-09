96434d3f58368c345fc78e0392ed99891a998998
Oksana Kolomenko <Oksana.Kolomenko@Student.HTW-Berlin.de>
2025-09-09 22:18:48 +0200

    cs h c23 em%Ste

--- run_setup.py
+++ run_setup.py
@@ -83,7 +83,7 @@ def run_txt_emb():
                   "pca_str": ""},
         
         """
-        # all summaries, metr features
+        """# all summaries, metr features
         "pca_conc2": {"X": X_metr,
                           "summaries": all_summaries,
                           "conc": "conc2",
@@ -100,7 +100,7 @@ def run_txt_emb():
                   "summaries": nom_summaries,
                   "conc": "conc3",
                   "pca": False,
-                  "pca_str": ""},
+                  "pca_str": ""},"""
         # all summaries, metr features
         "conc2": {"X": X_metr,
                   "summaries": all_summaries,
@@ -116,7 +116,7 @@ def run_txt_emb():
         #"all_miniLM_L6_v2": feature_extractor_all_minilm_l6_v2,
 
         # Stella en 400m v5
-        "Stella-EN-400M-v5": feature_extractor_stella_en_400M_v5,
+        #"Stella-EN-400M-v5": feature_extractor_stella_en_400M_v5,
 
         # GTR T5 Base
         #"GTR_T5_Base": feature_extractor_gtr_t5_base,
@@ -125,16 +125,16 @@ def run_txt_emb():
         #"sentence_t5_base": feature_extractor_sentence_t5_base,
 
         # Ember v1
-        "ember_v1": feature_extractor_ember_v1,
+        #"ember_v1": feature_extractor_ember_v1,
 
         # E5 Models
         #"E5-Small-V2": feature_extractor_e5_small_v2,
-        #"E5-Base-V2": feature_extractor_e5_base_v2,
+        "E5-Base-V2": feature_extractor_e5_base_v2,
         #"E5-Large-V2": feature_extractor_e5_large_v2,
 
         # BGE Models (done)
         #"BGE-Small-EN-v1.5": feature_extractor_bge_small_en_v1_5,
-        #"BGE-Base-EN-v1.5": feature_extractor_bge_base_en_v1_5,
+        "BGE-Base-EN-v1.5": feature_extractor_bge_base_en_v1_5,
         #"BGE-Large-EN-v1.5": feature_extractor_bge_large_en_v1_5,
 
         # GIST Models
@@ -143,10 +143,10 @@ def run_txt_emb():
         #"GIST-Large-Embedding-v0": feature_extractor_gist_large_embedding_v0,
 
         # GTE Models
-        #"GTE-Base": feature_extractor_gte_base,
+        "GTE-Base": feature_extractor_gte_base,
         #"GTE-Base-EN-v1.5": feature_extractor_gte_base_en_v1_5,
         #"GTE-Large": feature_extractor_gte_large,
-        #"GTE-Small": feature_extractor_gte_small,
+        "GTE-Small": feature_extractor_gte_small,
 
         # Potion Models
         # "Potion-Base-2M": feature_extractor_potion_base_2M,
