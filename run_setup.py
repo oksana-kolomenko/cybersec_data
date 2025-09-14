fe8e98585e97710ed83b6d903ecd45fe2b267fe3
Oksana Kolomenko <Oksana.Kolomenko@Student.HTW-Berlin.de>
2025-09-14 10:54:34 +0200

    cs conc1

--- run_setup.py
+++ run_setup.py
@@ -124,33 +124,33 @@ def run_txt_emb():
         #"sentence_t5_base": feature_extractor_sentence_t5_base,
 
         # Ember v1
-        "ember_v1": feature_extractor_ember_v1,
+        #"ember_v1": feature_extractor_ember_v1,
 
         # E5 Models
-        "E5-Small-V2": feature_extractor_e5_small_v2,
-        "E5-Base-V2": feature_extractor_e5_base_v2,
+        #"E5-Small-V2": feature_extractor_e5_small_v2,
+        #"E5-Base-V2": feature_extractor_e5_base_v2,
 
         # BGE Models (done)
-        "BGE-Small-EN-v1.5": feature_extractor_bge_small_en_v1_5,
+        #"BGE-Small-EN-v1.5": feature_extractor_bge_small_en_v1_5,
         #"BGE-Base-EN-v1.5": feature_extractor_bge_base_en_v1_5,
 
         # GIST Models
-        "GIST-Small-Embedding-v0": feature_extractor_gist_small_embedding_v0,
+        #"GIST-Small-Embedding-v0": feature_extractor_gist_small_embedding_v0,
         #"GIST-Embedding-v0": feature_extractor_gist_embedding_v0,
 
         # GTE Models
-        "GTE-Small": feature_extractor_gte_small,
-        "GTE-Base": feature_extractor_gte_base,
-        "GTE-Base-EN-v1.5": feature_extractor_gte_base_en_v1_5,
+        #"GTE-Small": feature_extractor_gte_small,
+        #"GTE-Base": feature_extractor_gte_base,
+        #"GTE-Base-EN-v1.5": feature_extractor_gte_base_en_v1_5,
 
         # large
-        "E5-Large-V2": feature_extractor_e5_large_v2,
-        "BGE-Large-EN-v1.5": feature_extractor_bge_large_en_v1_5,
-        #"GIST-Large-Embedding-v0": feature_extractor_gist_large_embedding_v0,
-        #"GTE-Large": feature_extractor_gte_large,
+        #"E5-Large-V2": feature_extractor_e5_large_v2,
+        #"BGE-Large-EN-v1.5": feature_extractor_bge_large_en_v1_5,
+        "GIST-Large-Embedding-v0": feature_extractor_gist_large_embedding_v0,
+        "GTE-Large": feature_extractor_gte_large,
 
         # Stella en 400m v5
-        #"Stella-EN-400M-v5": feature_extractor_stella_en_400M_v5,
+        "Stella-EN-400M-v5": feature_extractor_stella_en_400M_v5,
 
         # Potion Models
         # "Potion-Base-2M": feature_extractor_potion_base_2M,
